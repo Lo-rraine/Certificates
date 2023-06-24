e23149ae6309872eb5feaaca77ea99848c039ee5
Lo-rraine <65586580+Lo-rraine@users.noreply.github.com>
2023-06-24 19:58:16 +0200

    swagger

--- Certificates/Program.cs
+++ Certificates/Program.cs
@@ -22,13 +22,13 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
